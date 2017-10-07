60dc6f8e67e740d25d102b2d1c3bbdec629ca655
rnpatel17 <ripal_1984@ymail.com>
2017-10-07 14:04:24 -0400

    change in base controller

--- IOT.Philips.WebAPI/Controllers/BaseController.cs
+++ IOT.Philips.WebAPI/Controllers/BaseController.cs
@@ -12,7 +12,9 @@ namespace IOT.Philips.WebAPI.Controllers
     {
        
         public const string BRIDGE_IP = "192.168.20.106";///"192.168.1.225";//"192.168.0.8";// "192.168.0.1";
+            //Ip of philips hue device
         public const string APP_ID = "hb7b215e002b8b8f";// "1d0de40d14861c71a21c0c923d8a9e3";//"001788fffe7b215e";
+        // app id of philips bridge 
 
 
     }
